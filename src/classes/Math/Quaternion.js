ab5cf0966b1165a8eca5cd0b920c5bba688c81b6
Anton Zhuravsky <mail2lf@gmail.com>
2019-01-19 15:07:19 +0300

    Adding helper methods to quaternions

--- src/classes/Math/Quaternion.js
+++ src/classes/Math/Quaternion.js
@@ -6,6 +6,8 @@ Goblin.Quaternion = function( x, y, z, w ) {
 	this.normalize();
 };
 
+Goblin.Quaternion.IDENTITY = new Goblin.Quaternion();
+
 Goblin.Quaternion.prototype = {
 	set: function( x, y, z, w ) {
 		this.x = x;
@@ -19,6 +21,10 @@ Goblin.Quaternion.prototype = {
 		this.y = q.y;
 		this.z = q.z;
 		this.w = q.w;
+	},
+
+	toString: function () {
+		return '(' + this.x + ', ' + this.y + ', ' + this.z + ', ' + this.w + ')';
 	},
 
 	multiply: function( q ) {
@@ -99,6 +105,95 @@ Goblin.Quaternion.prototype = {
 		dest.y = iy * qw + iw * -qy + iz * -qx - ix * -qz;
 		dest.z = iz * qw + iw * -qz + ix * -qy - iy * -qx;
 	},
+
+	setFromMat4: function (m) {
+        var m00, m01, m02, m10, m11, m12, m20, m21, m22,
+            tr, s, rs, lx, ly, lz;
+
+        // Cache matrix values for super-speed
+        m00 = m.e00;
+        m01 = m.e10;
+        m02 = m.e20;
+        m10 = m.e01;
+        m11 = m.e11;
+        m12 = m.e21;
+        m20 = m.e02;
+        m21 = m.e12;
+        m22 = m.e22;
+
+        // Remove the scale from the matrix
+        lx = 1 / Math.sqrt(m00 * m00 + m01 * m01 + m02 * m02);
+        ly = 1 / Math.sqrt(m10 * m10 + m11 * m11 + m12 * m12);
+        lz = 1 / Math.sqrt(m20 * m20 + m21 * m21 + m22 * m22);
+
+        m00 *= lx;
+        m01 *= lx;
+        m02 *= lx;
+        m10 *= ly;
+        m11 *= ly;
+        m12 *= ly;
+        m20 *= lz;
+        m21 *= lz;
+        m22 *= lz;
+
+        // http://www.cs.ucr.edu/~vbz/resources/quatut.pdf
+
+        tr = m00 + m11 + m22;
+        if (tr >= 0) {
+            s = Math.sqrt(tr + 1);
+            this.w = s * 0.5;
+            s = 0.5 / s;
+            this.x = (m12 - m21) * s;
+            this.y = (m20 - m02) * s;
+            this.z = (m01 - m10) * s;
+        } else {
+            if (m00 > m11) {
+                if (m00 > m22) {
+                    // XDiagDomMatrix
+                    rs = (m00 - (m11 + m22)) + 1;
+                    rs = Math.sqrt(rs);
+
+                    this.x = rs * 0.5;
+                    rs = 0.5 / rs;
+                    this.w = (m12 - m21) * rs;
+                    this.y = (m01 + m10) * rs;
+                    this.z = (m02 + m20) * rs;
+                } else {
+                    // ZDiagDomMatrix
+                    rs = (m22 - (m00 + m11)) + 1;
+                    rs = Math.sqrt(rs);
+
+                    this.z = rs * 0.5;
+                    rs = 0.5 / rs;
+                    this.w = (m01 - m10) * rs;
+                    this.x = (m20 + m02) * rs;
+                    this.y = (m21 + m12) * rs;
+                }
+            } else if (m11 > m22) {
+                // YDiagDomMatrix
+                rs = (m11 - (m22 + m00)) + 1;
+                rs = Math.sqrt(rs);
+
+                this.y = rs * 0.5;
+                rs = 0.5 / rs;
+                this.w = (m20 - m02) * rs;
+                this.z = (m12 + m21) * rs;
+                this.x = (m10 + m01) * rs;
+            } else {
+                // ZDiagDomMatrix
+                rs = (m22 - (m00 + m11)) + 1;
+                rs = Math.sqrt(rs);
+
+                this.z = rs * 0.5;
+                rs = 0.5 / rs;
+                this.w = (m01 - m10) * rs;
+                this.x = (m20 + m02) * rs;
+                this.y = (m21 + m12) * rs;
+            }
+        }
+
+        return this;
+    },
 
 	angleBetween: function( q ) {
 		/*_tmp_quat4_1.invertQuaternion( this );
