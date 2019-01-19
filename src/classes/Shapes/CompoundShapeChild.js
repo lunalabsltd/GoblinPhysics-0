a1f8e78bd877e9027ec6061ae73874339b29eaec
Anton Zhuravsky <mail2lf@gmail.com>
2019-01-19 15:06:29 +0300

    Enabling compound shape with center of mass

--- src/classes/Shapes/CompoundShapeChild.js
+++ src/classes/Shapes/CompoundShapeChild.js
@@ -2,17 +2,27 @@
  * @class CompoundShapeChild
  * @constructor
  */
-Goblin.CompoundShapeChild = function( shape, position, rotation ) {
+Goblin.CompoundShapeChild = function( shape, local_position, rotation ) {
 	this.shape = shape;
 
-	this.position = new Goblin.Vector3( position.x, position.y, position.z );
+	this.local_position = new Goblin.Vector3( local_position.x, local_position.y, local_position.z );
+    this.center_of_mass = new Goblin.Vector3();
+    this.position = new Goblin.Vector3();
 	this.rotation = new Goblin.Quaternion( rotation.x, rotation.y, rotation.z, rotation.w );
 
 	this.transform = new Goblin.Matrix4();
 	this.transform_inverse = new Goblin.Matrix4();
-	this.transform.makeTransform( this.rotation, this.position );
-	this.transform.invertInto( this.transform_inverse );
-
+	
 	this.aabb = new Goblin.AABB();
-	this.aabb.transform( this.shape.aabb, this.transform );
+
+    this.updateDerived();
+};
+
+Goblin.CompoundShapeChild.prototype.updateDerived = function () {
+    this.position.copy( this.local_position );
+    this.position.subtract( this.center_of_mass );
+
+    this.transform.makeTransform( this.rotation, this.position );
+    this.transform.invertInto( this.transform_inverse );
+    this.aabb.transform( this.shape.aabb, this.transform );
 };
