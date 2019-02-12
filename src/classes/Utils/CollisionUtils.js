2a58ba3b4b1a6d042bac99d8f3ac573f4ebcea86
Anton Zhuravsky <mail2lf@gmail.com>
2019-02-12 23:52:04 +0300

    Disabling static-static collision pairs

--- src/classes/Utils/CollisionUtils.js
+++ src/classes/Utils/CollisionUtils.js
@@ -3,6 +3,11 @@ Goblin.CollisionUtils = {};
 Goblin.CollisionUtils.canBodiesCollide = function( object_a, object_b ) {
 	var matrix = object_a.world.collision_matrix;
 
+    if ( object_a._is_static && object_b._is_static ) {
+        // static bodies should never collide
+        return false;
+    }
+
 	if ( matrix[ object_a.layer ] && matrix[ object_a.layer ][ object_b.layer ] === false ) {
 		return false;
 	} else {
