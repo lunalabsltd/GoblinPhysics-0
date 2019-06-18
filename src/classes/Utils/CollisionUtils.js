049a2071b2cf68b374da50968194e6ab0bbe9991
Anton Zhuravsky <mail2lf@gmail.com>
2019-06-18 11:36:44 +0300

    Cherry-picking e20e33e into feature/PB-77

--- src/classes/Utils/CollisionUtils.js
+++ src/classes/Utils/CollisionUtils.js
@@ -7,6 +7,11 @@ Goblin.CollisionUtils.canBodiesCollide = function( object_a, object_b ) {
 
     var matrix = object_a.world.collision_matrix;
 
+    if ( ( object_a._is_static || object_a._is_kinematic ) && ( object_b._is_static || object_b._is_kinematic ) ) {
+        // static bodies should never collide
+        return false;
+    }
+
     if ( matrix[ object_a.layer ] && matrix[ object_a.layer ][ object_b.layer ] === false ) {
         return false;
     } else {
